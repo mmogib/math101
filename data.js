b8a4175376bd1bf95fbb6a099c48665a0464620f
Mohammed Alshahrani <mmogib@gmail.com>
2023-08-12 16:27:57 +0300

    add classwork with query string

--- data.js
+++ data.js
@@ -2,14 +2,21 @@ function showGrades() {
     return {
         isLoading: false,
         fullname: null,
+        key: null,
+        kfupm_id:null,
         scores: [],
-        getScores(id) {
-            if (String(id).length === 9) {
-
+        init(){
+            const urlParams = new URLSearchParams(window.location.search);
+            const myParam = urlParams.get('key');
+            this.key= myParam
+            this.getScores()
+        },
+        getScores() {
                 this.isLoading = true
                 this.scores = []
                 this.fullname = null
-                fetch(`https://api.airtable.com/v0/appLKzXucWNq7Ns0j/Grades?fields%5B%5D=QUIZ+1&fields%5B%5D=QUIZ+2&fields%5B%5D=QUIZ+3&fields%5B%5D=QUIZ+4&fields%5B%5D=QUIZ+5&fields%5B%5D=QUIZ+6&fields%5B%5D=BEST+4&fields%5B%5D=CLW+(NORMALIZED)&fields%5B%5D=Full+Name&filterByFormula=ID%3D${id}&maxRecords=20`, {
+                const id = this.key
+                fetch(`https://api.airtable.com/v0/appLKzXucWNq7Ns0j/Grades?fields%5B%5D=ID&fields%5B%5D=QUIZ+1&fields%5B%5D=QUIZ+2&fields%5B%5D=QUIZ+3&fields%5B%5D=QUIZ+4&fields%5B%5D=QUIZ+5&fields%5B%5D=QUIZ+6&fields%5B%5D=BEST+4&fields%5B%5D=CLW+(NORMALIZED)&fields%5B%5D=Full+Name&filterByFormula=HID%3D%22${id}%22&maxRecords=20`, {
                     headers: {
                         Authorization: "Bearer key5fxKsZ0cka9x9D",
                     }
@@ -18,8 +25,10 @@ function showGrades() {
                 ).then(data => {
                     this.isLoading = false
                     const flds = data.records[0].fields
-                    const names = Object.keys(flds).sort().filter(n=>n!=='Full Name')
+                    const names = Object.keys(flds).sort().filter(n=>n!=='Full Name' && n!=="ID")
                     this.fullname = flds['Full Name']
+                    this.kfupm_id = flds['ID']
+                    
                     this.scores = names.map(n => {
                         value = Array.isArray(flds[n]) ? flds[n][0] : flds[n];
                         return n !== 'Full Name' && { name: n, value }
@@ -29,5 +38,5 @@ function showGrades() {
             }
 
         }
-    }
+    
 }
