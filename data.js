1eac048a9d4ff525ec084072afae698b7006a631
Mohammed Alshahrani <mmogib@gmail.com>
2023-08-13 06:07:45 +0300

    added new pink h1

--- data.js
+++ data.js
@@ -18,7 +18,7 @@ function showGrades() {
                 const id = this.key
                 fetch(`https://api.airtable.com/v0/appLKzXucWNq7Ns0j/Grades?fields%5B%5D=ID&fields%5B%5D=QUIZ+1&fields%5B%5D=QUIZ+2&fields%5B%5D=QUIZ+3&fields%5B%5D=QUIZ+4&fields%5B%5D=QUIZ+5&fields%5B%5D=QUIZ+6&fields%5B%5D=BEST+4&fields%5B%5D=CLW+(NORMALIZED)&fields%5B%5D=Full+Name&filterByFormula=HID%3D%22${id}%22&maxRecords=20`, {
                     headers: {
-                        Authorization: "Bearer key5fxKsZ0cka9x9D",
+                        Authorization: "Bearer patS1tloTkU4PXgRN.5abcc400a57b06eb826e629ca497ec3055af4c437da7d6ff91902e060a4bb4f5",
                     }
                 }).then(
                     resp => resp.json()
